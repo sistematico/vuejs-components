1f621751414ae672bfdb3a8bfa31441a654fa27e
Lucas Saliés Brum <sistematico@gmail.com>
2022-10-02 15:31:39 -0400

    Added windicss

--- src/App.vue
+++ src/App.vue
@@ -6,7 +6,7 @@ import Menu from './components/menu.vue'
   <nav class="bg-gray-800 p-2 mt-0 fixed w-full z-10 top-0">
     <div class="container mx-auto flex flex-wrap items-center">
       <div class="flex w-full md:w-1/2 justify-center md:justify-start text-white font-extrabold">
-        <a class="animation text-gray-500 no-underline hover:text-white hover:no-underline" href="#">
+        <a class="transition-all duration-500 ease-in-out text-gray-500 no-underline hover:text-white hover:no-underline" href="#">
           <span class="text-2xl pl-2">
             Vue Components
           </span>
--- src/components/menu.vue
+++ src/components/menu.vue
@@ -4,18 +4,29 @@ import { RouterLink } from 'vue-router'
 <template>
   <ul class="list-reset flex justify-between flex-1 md:flex-none items-center">
     <li class="mr-3">
-      <a class="inline-block py-2 px-4 text-white no-underline" href="#">Active</a>
-    </li>
-    <li class="mr-3">
-      <RouterLink to="/" class="inline-block text-gray-600 no-underline hover:text-gray-200 hover:text-underline py-2 px-4">
+      <RouterLink 
+        to="/" 
+        class="inline-block text-gray-600 no-underline hover:text-gray-200 hover:text-underline py-2 px-4
+          transition-all duration-700 ease-in-out"
+      >
         Início
       </RouterLink>
     </li>
     <li class="mr-3">
-      <RouterLink to="/about" class="inline-block text-gray-600 no-underline hover:text-gray-200 hover:text-underline py-2 px-4">
+      <RouterLink 
+        to="/about" 
+        class="inline-block text-gray-600 no-underline hover:text-gray-200 hover:text-underline py-2 px-4"
+      >
         Sobre
       </RouterLink>
-      <a  href="#">link</a>
     </li>
   </ul>
 </template>
+<style scoped>
+
+.router-link-active,
+.router-link-exact-active {
+   color: #fff;
+ }
+
+</style>
